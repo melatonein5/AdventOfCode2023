4ed19e88483c71774b256cd99d57dffbfb6babba
Joseph McCallum-Nattrass <jmn@jmnetwork.uk>
2023-12-22 02:18:35 +0000

    Added boilerplate file

---
+++ bolierplate.go
@@ -0,0 +1,31 @@
+package boilerplate
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+//Contains the boilerplate code for the solutions, copy and paste this into the solution files
+//At some point it should probably be its own package, but I'm not sure how to do that yet
+
+// loadInputs loads from a txt file and returns a slice of strings, each string is a line from the txt file. The txt file is called inputs.txt
+func loadInputs() []string {
+	var inputs []string
+	//Open inputs.txt
+	file, err := os.Open("inputs.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Close file when function ends
+	defer file.Close()
+
+	//Read file line by line and add each line to inputs
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputs = append(inputs, scanner.Text())
+	}
+
+	//Return inputs
+	return inputs
+}
